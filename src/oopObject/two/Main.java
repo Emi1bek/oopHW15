b46e671471e23af0ef73d5de3fee8dbf662b0658
user <emilbek.ergeshali@gmail.com>
2023-01-20 21:14:17 +0300

    commt1

--- src/oopObject/two/Main.java
+++ src/oopObject/two/Main.java
@@ -8,13 +8,18 @@ public class Main {
                 new Turtle(),
                 new Eagle()
         };
+        Shark shar = new Shark();
+        System.out.println(shar.getClass().getName().equals("Turtle"));// == false болуп атат тушунбой атам эмнеге
 
         for (Animal i: animals) {
             if(i instanceof Shark){
                 ((Shark)i).attack();
-            } else if ( i instanceof Turtle/*i.getClass().getName().equals("Turtle")*/) {
+            } else if (i.getClass().getName().equals("Turtle")) {
                 ((Turtle)i).swim();
-            } else if (i instanceof Eagle) {
+                /*Че то интел айж нин жаны версиясында
+                башкача ищтейт окшойт сабакка караганда true
+                болуш керек эле но false болуп атат*/
+            } else if (i.getClass().getName().equals("Eagle")) {
                 ((Eagle)i).fly();
             }else{
                 System.out.println("Нет такого класса!");
